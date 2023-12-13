e8e9384565ade5bd1c5f6a32cab7f915e78e5c4a
RuslanPidhainyi <ruslanpidgauny@gmail.com>
2023-12-13 16:52:32 +0100

    Register design

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,34 @@
 import "./register.scss";
 
 const Register = () => {
-  return <div className="register">Register</div>;
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Lama Social.</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates
+            adipisci maiores atque sit numquam. Eaque, asperiores! Quis dolorem
+            nisi, iusto optio fugit perferendis et ullam, asperiores error
+            quibusdam qui possimus.
+          </p>
+          <span>Do you have an account?</span>
+          <button>Login</button>
+        </div>
+
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Name" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
