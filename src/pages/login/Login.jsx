172ed9278f6c9c759f84718e1360e3916b77c1e7
RuslanPidhainyi <ruslanpidgauny@gmail.com>
2023-12-13 16:49:37 +0100

    Login design(Made in HTML & SCSS/(sass))

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,7 +2,32 @@
 import "./login.scss";
 
 const Login = () => {
-  return <div className="login">Login</div>;
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates
+            adipisci maiores atque sit numquam. Eaque, asperiores! Quis dolorem
+            nisi, iusto optio fugit perferendis et ullam, asperiores error
+            quibusdam qui possimus.
+          </p>
+          <span>Don't you have an account?</span>
+          <button>Register</button>
+        </div>
+
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
