16cf9c5fb355b19ad9134248c717d0e40e21a7fe
RuslanPidhainyi <ruslanpidgauny@gmail.com>
2023-12-19 00:13:02 +0100

    rightBar menu design

--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -1,7 +1,104 @@
 import "./rightBar.scss";
+import ClearIcon from '@mui/icons-material/Clear';
 
 const RightBar = () => {
-  return <div className="rightBar">RightBar</div>;
+  return <div className="rightBar">
+      <div className="container">
+        <div className="item">
+          <span>Suggestions For You </span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/19137934/pexels-photo-19137934/free-photo-of-woman-in-lace-white-top-sitting-on-step-by-door.jpeg?auto=compress& cs=tinysrgb&w=600&lazy=load" alt="" />
+              <span>Jane Doe</span>
+            </div>
+            <div className="buttons" >
+                <button>follow</button>
+                <button><ClearIcon/></button>
+              </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/19137934/pexels-photo-19137934/free-photo-of-woman-in-lace-white-top-sitting-on-step-by-door.jpeg?auto=compress& cs=tinysrgb&w=600&lazy=load" alt="" />
+              <span>Jane Doe</span>
+            </div>
+            <div className="buttons" >
+                <button>follow</button>
+                <button><ClearIcon/></button>
+              </div>
+          </div>
+        </div>
+        <div className="item">
+          <span>Latest Activites</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/19137934/pexels-photo-19137934/free-photo-of-woman-in-lace-white-top-sitting-on-step-by-door.jpeg?auto=compress& cs=tinysrgb&w=600&lazy=load" alt="" />
+              <p>
+                <span>Jane Doe</span> changed theit cover picture 
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/19137934/pexels-photo-19137934/free-photo-of-woman-in-lace-white-top-sitting-on-step-by-door.jpeg?auto=compress& cs=tinysrgb&w=600&lazy=load" alt="" />
+              <p>
+                <span>Jane Doe</span> changed theit cover picture 
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/19137934/pexels-photo-19137934/free-photo-of-woman-in-lace-white-top-sitting-on-step-by-door.jpeg?auto=compress& cs=tinysrgb&w=600&lazy=load" alt="" />
+              <p>
+                <span>Jane Doe</span> changed theit cover picture 
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/19137934/pexels-photo-19137934/free-photo-of-woman-in-lace-white-top-sitting-on-step-by-door.jpeg?auto=compress& cs=tinysrgb&w=600&lazy=load" alt="" />
+              <p>
+                <span>Jane Doe</span> changed theit cover picture 
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="item">
+          <span>Online Friends</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/19137934/pexels-photo-19137934/free-photo-of-woman-in-lace-white-top-sitting-on-step-by-door.jpeg?auto=compress& cs=tinysrgb&w=600&lazy=load" alt="" />
+              <div className="online" />
+              <span>Jane Doe</span> 
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/19137934/pexels-photo-19137934/free-photo-of-woman-in-lace-white-top-sitting-on-step-by-door.jpeg?auto=compress& cs=tinysrgb&w=600&lazy=load" alt="" />
+              <div className="online" />
+              <span>Jane Doe</span> 
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/19137934/pexels-photo-19137934/free-photo-of-woman-in-lace-white-top-sitting-on-step-by-door.jpeg?auto=compress& cs=tinysrgb&w=600&lazy=load" alt="" />
+              <div className="online" />
+              <span>Jane Doe</span> 
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/19137934/pexels-photo-19137934/free-photo-of-woman-in-lace-white-top-sitting-on-step-by-door.jpeg?auto=compress& cs=tinysrgb&w=600&lazy=load" alt="" />
+              <div className="online" />
+              <span>Jane Doe</span> 
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>;
 };
 
 export default RightBar;
